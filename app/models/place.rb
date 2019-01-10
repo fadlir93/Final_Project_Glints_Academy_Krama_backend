3fdb016e2e48b0f3ad0c9dc5d5a4da9d3ce135a8
husnul23 <batalkawin.hf@gmail.com>
2019-01-10 20:29:55 +0700

    fix pipeline error

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Place < ApplicationRecord
     has_one_attached :image
     has_many :rulesplace
@@ -6,15 +5,4 @@ class Place < ApplicationRecord
     validates :title, presence: true
     validates :desc, presence: true
     validates :img, presence:true
-=======
-# frozen_string_literal: true
-
-class Place < ApplicationRecord
-  has_one_attached :image
-  has_many :rulesplace
-  has_many :rules, through: :rulesplace
-  validates :title, presence: true
-  validates :desc, presence: true
-  mount_uploader :image, ImageUploader
->>>>>>> e5de5a8ab7ab55e24d63e1bdab063a587fa42bc5
 end
